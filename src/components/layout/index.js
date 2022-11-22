afeb78db577ba31795890fb9cb6363c56ec08d5c
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2022-11-22 22:55:52 +0200

    update seo

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -92,7 +92,9 @@ export const Layout = (props) => {
       <Helmet>
         <title>Admin Dashboard</title>
         <meta name="description" content="ClinicPlus offers comprehensive Occupational Health Management and Consulting service to mines and industries. Our goal is to help our clients manage their occupational health and safety risks." />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/cp-logo-full.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/cp-logo-full.png" />
+        <link href="/cp-logo-full.png" rel="icon" />
+        <link href="/cp-logo-full.png" rel="apple-touch-icon" />
         <link href="/vendor/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
       </Helmet>
