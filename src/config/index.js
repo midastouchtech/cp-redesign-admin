ae87993d1dc7f1c15ca49edd5746744a18c18654
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2023-03-08 11:30:55 +0200

    update pricing

--- src/config/index.js
+++ src/config/index.js
@@ -1,25 +1,25 @@
 export const CLINIC_LOCATIONS = ["Hendrina", "Churchill"];
 export const MEDICAL_SERVICES = {
   "medical-examination": {
-    price: 495.79,
+    price: 525.54,
     title: "Medical Examination",
     id: "medical-examination",
     info: `Medical assessment means a visual and physical inspection of the consumer, noting deviations from the norm, and a statement of the consumer's mental and physical condition that can be amendable to or resolved by appropriate actions of the provider.`,
   },
   "x-ray": {
-    price: 217.67,
+    price: 230.73,
     title: "X - Rays",
     id: "x-ray",
     info: `An X-ray is a quick, painless test that produces images of the structures inside your body — particularly your bones. X-ray beams pass through your body, and they are absorbed in different amounts depending on the density of the material they pass through`,
   },
   "6-in-1-drug-test": {
-    price: 111.06,
+    price: 117.72,
     title: "6 in 1 Drug Test",
     id: "6-in-1-drug-test",
     info: `Our 6-test panel drug test uses urine to check for the presence of drug abuse. In comparison to our 10-panel drug test, this test covers the most popular illicit drugs, also known as “street drugs” which are rarely prescribed.`,
   },
   cannabis: {
-    price: 37.43,
+    price: 39.68,
     title: "Canbabis/Dagga",
     id: "cannabis",
     info: `Cannabis drug testing describes various drug test methodologies for the use of cannabis in medicine, sport, and law. Cannabis use is highly detectable and can be detected by urinalysis, hair analysis, as well as saliva tests for days or weeks.`,
@@ -31,31 +31,31 @@ export const MEDICAL_SERVICES = {
     info: `In human resource development, induction training is a form of introduction for new employees in order to enable them to do their work in a new profession or job role within an organisation. Training can be systematic or unsystematic training. Induction training is systematic training`,
   },
   "pregnancy-test": {
-    price: 39.12,
+    price: 41.47,
     title: "Pregnancy Test",
     id: "pregnancy-test",
     info: "A pregnancy test is used to determine whether a woman is pregnant. The two primary methods are: 1 testing for human pregnancy hormone in blood or urine and 2 ultrasonography. Testing blood for hCG results in the earliest detection of pregnancy.",
   },
   "sugar-test": {
-    price: 38.67,
+    price: 40.99,
     title: "Sugar Test",
     id: "sugar-test",
     info: "Many types of glucose tests exist and they can be used to estimate blood sugar levels at a given time or, over a longer period of time, to obtain average levels or to see how fast body is able to normalize changed glucose levels. Eating food for example leads to elevated blood sugar levels. Wikipedia",
   },
   clearance: {
-    price: 445,
+    price: 226.71,
     title: "Clearance",
     id: "clearance",
     info: "What is the creatinine clearance test? The creatinine clearance test is an older test used to check your kidney function. This test allows your healthcare provider to look at samples of your urine and blood to see how much of the waste product — creatinine — is filtered out by your kidneys",
   },
   "vienna-test": {
-    price: 670.45,
+    price: 710.70,
     title: "Dover Test",
     id: "vienna-test",
     info: "The test is a valuable preventative measure for employees working with moving machinery, staff operating vehicles and office staff. Skills are assessed as they currently exist. ... A computerised assessment report will give an indication on corrective measures to be taken into account.",
   },
   hiv: {
-    price: 117.37,
+    price: 124.41,
     title: "HIV",
     id: "hiv",
     info: "HIV tests are used to detect the presence of the human immunodeficiency virus, the virus that causes acquired immunodeficiency syndrome, in serum, saliva, or urine. Such tests may detect antibodies, antigens, or RNA. ",
@@ -72,23 +72,23 @@ export const MEDICAL_SERVICES = {
     id: "first-aid-training",
     info: "When you understand first aid, you'll gain the ability to help those in need. Through our first aid certification classes you'll learn how to respond to specific situations, which will help you care for people in crisis as they wait for medical professionals to arrive.",
   },
-  "covid-19-rapid-test": {
-    price: 1100,
-    title: "Covid-19 Rapid Test",
-    id: "covid-19-rapid-test",
+  "full-exit-medical": {
+    price: 756.27,
+    title: "Full Exit Medical - medical older than 6 months",
+    id: "full-exit-medical",
+    info: "saldkjlsd",
+  },
+  "short-exit-medical": {
+    price: 286.12,
+    title: "Short Exit Medical - medical less than 6 months",
+    id: "short-exit-medical",
     info: "saldkjlsd",
   },
   "covid-19-screen-questionnaire": {
-    price: 89.36,
+    price: 94.72,
     title: "Covid-19 Questionnaire",
     id: "covid-19-screen-questionnaire",
     info: "This is the first COVID-19 antibody test that has been approved for use in South Africa. The specificity of this test is 100% and it has a sensitivity of 95.8% for samples collected 10 days or more after the onset of symptoms or a positive PCR (nasal test) result (McAdam, 2020).",
-  },
-  exit: {
-    price: 713.46,
-    title: "Exit",
-    id: "exit",
-    info: "Exit",
   },
   other: {
     price: 100,
