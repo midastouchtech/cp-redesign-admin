b7640882fbcac5b62d445f219cdf036acf174b72
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2023-04-06 23:51:32 +0200

    fix currency

--- src/views/analytics/index.js
+++ src/views/analytics/index.js
@@ -381,7 +381,7 @@ const Analytics = ({ socket }) => {
                     <div>
                       <p className="fs-14 text-black mb-1 bold">Net Pay</p>
                       <span className="fs-30 text-black font-w600">
-                        {analytics && new Intl.NumberFormat('en-US', { style: 'currency', currency: 'ZAR' }).format(getValues(
+                        {analytics && new Intl.NumberFormat('en-ZA', { style: 'currency', currency: 'ZAR' }).format(getValues(
                               analytics?.allClinics?.amountsMade
                             ).reduce((acc, curr) => curr + acc, 0))}
 
