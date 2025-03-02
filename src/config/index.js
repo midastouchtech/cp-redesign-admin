1526371f6b7a2627069c97710276276846595e2f
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-03-02 17:10:12 +0200

    update pricing

--- src/config/index.js
+++ src/config/index.js
@@ -1,77 +1,77 @@
-export const CLINIC_LOCATIONS = ["Hendrina", "Churchill"];
-export const DOVER_PRICE = 753.34
-export const XRAYS_PRICE = 230.72
+export const CLINIC_LOCATIONS = ['Hendrina', 'Churchill'];
+export const DOVER_PRICE = 828.67;
+export const XRAYS_PRICE = 249.18;
 export const MEDICAL_SERVICES = {
-  "medical-examination": {
-    price: 557.07,
-    title: "Medical Examination",
-    id: "medical-examination",
+  'medical-examination': {
+    price: 601.64,
+    title: 'Medical Examination',
+    id: 'medical-examination',
     info: `Medical assessment means a visual and physical inspection of the consumer, noting deviations from the norm, and a statement of the consumer's mental and physical condition that can be amendable to or resolved by appropriate actions of the provider.`,
   },
-  "6-in-1-drug-test": {
-    price: 127.13,
-    title: "6 in 1 Drug Test",
-    id: "6-in-1-drug-test",
+  '6-in-1-drug-test': {
+    price: 137.3,
+    title: '6 in 1 Drug Test',
+    id: '6-in-1-drug-test',
     info: `Our 6-test panel drug test uses urine to check for the presence of drug abuse. In comparison to our 10-panel drug test, this test covers the most popular illicit drugs, also known as “street drugs” which are rarely prescribed.`,
   },
   cannabis: {
-    price: 42.85,
-    title: "Canbabis/Dagga",
-    id: "cannabis",
+    price: 46.28,
+    title: 'Canbabis/Dagga',
+    id: 'cannabis',
     info: `Cannabis drug testing describes various drug test methodologies for the use of cannabis in medicine, sport, and law. Cannabis use is highly detectable and can be detected by urinalysis, hair analysis, as well as saliva tests for days or weeks.`,
   },
   induction: {
-    price: 280.37,
-    title: "Induction",
-    id: "induction",
+    price: 302.8,
+    title: 'Induction',
+    id: 'induction',
     info: `In human resource development, induction training is a form of introduction for new employees in order to enable them to do their work in a new profession or job role within an organisation. Training can be systematic or unsystematic training. Induction training is systematic training`,
   },
-  "pregnancy-test": {
-    price: 44.79,
-    title: "Pregnancy Test",
-    id: "pregnancy-test",
-    info: "A pregnancy test is used to determine whether a woman is pregnant. The two primary methods are: 1 testing for human pregnancy hormone in blood or urine and 2 ultrasonography. Testing blood for hCG results in the earliest detection of pregnancy.",
+  'pregnancy-test': {
+    price: 48.36,
+    title: 'Pregnancy Test',
+    id: 'pregnancy-test',
+    info: 'A pregnancy test is used to determine whether a woman is pregnant. The two primary methods are: 1 testing for human pregnancy hormone in blood or urine and 2 ultrasonography. Testing blood for hCG results in the earliest detection of pregnancy.',
   },
-  "sugar-test": {
-    price: 44.28,
-    title: "Sugar Test",
-    id: "sugar-test",
-    info: "Many types of glucose tests exist and they can be used to estimate blood sugar levels at a given time or, over a longer period of time, to obtain average levels or to see how fast body is able to normalize changed glucose levels. Eating food for example leads to elevated blood sugar levels. Wikipedia",
+  'sugar-test': {
+    price: 47.82,
+    title: 'Sugar Test',
+    id: 'sugar-test',
+    info: 'Many types of glucose tests exist and they can be used to estimate blood sugar levels at a given time or, over a longer period of time, to obtain average levels or to see how fast body is able to normalize changed glucose levels. Eating food for example leads to elevated blood sugar levels. Wikipedia',
   },
   clearance: {
-    price: 240.31,
-    title: "Clearance",
-    id: "clearance",
-    info: "What is the creatinine clearance test? The creatinine clearance test is an older test used to check your kidney function. This test allows your healthcare provider to look at samples of your urine and blood to see how much of the waste product — creatinine — is filtered out by your kidneys",
+    price: 259.55,
+    title: 'Clearance',
+    id: 'clearance',
+    info: 'What is the creatinine clearance test? The creatinine clearance test is an older test used to check your kidney function. This test allows your healthcare provider to look at samples of your urine and blood to see how much of the waste product — creatinine — is filtered out by your kidneys',
   },
   hiv: {
-    price: 134.36,
-    title: "HIV",
-    id: "hiv",
-    info: "HIV tests are used to detect the presence of the human immunodeficiency virus, the virus that causes acquired immunodeficiency syndrome, in serum, saliva, or urine. Such tests may detect antibodies, antigens, or RNA. ",
+    price: 145.1,
+    title: 'HIV',
+    id: 'hiv',
+    info: 'HIV tests are used to detect the presence of the human immunodeficiency virus, the virus that causes acquired immunodeficiency syndrome, in serum, saliva, or urine. Such tests may detect antibodies, antigens, or RNA. ',
   },
-  "full-exit-medical": {
-    price: 557.07,
-    title: "Full Exit Medical - medical older than 6 months",
-    id: "full-exit-medical",
-    info: "saldkjlsd",
+  'full-exit-medical': {
+    price: 601.64,
+    title: 'Full Exit Medical - medical older than 6 months',
+    id: 'full-exit-medical',
+    info: 'saldkjlsd',
   },
-  "short-exit-medical": {
-    price: 303.28,
-    title: "Short Exit Medical - medical less than 6 months",
-    id: "short-exit-medical",
-    info: "saldkjlsd",
+  'short-exit-medical': {
+    price: 327.54,
+    title: 'Short Exit Medical - medical less than 6 months',
+    id: 'short-exit-medical',
+    info: 'saldkjlsd',
   },
-  "covid-19-screen-questionnaire": {
-    price: 102.29,
-    title: "Covid-19 Questionnaire",
-    id: "covid-19-screen-questionnaire",
-    info: "This is the first COVID-19 antibody test that has been approved for use in South Africa. The specificity of this test is 100% and it has a sensitivity of 95.8% for samples collected 10 days or more after the onset of symptoms or a positive PCR (nasal test) result (McAdam, 2020).",
+  'covid-19-screen-questionnaire': {
+    price: 110.47,
+    title: 'Covid-19 Questionnaire',
+    id: 'covid-19-screen-questionnaire',
+    info: 'This is the first COVID-19 antibody test that has been approved for use in South Africa. The specificity of this test is 100% and it has a sensitivity of 95.8% for samples collected 10 days or more after the onset of symptoms or a positive PCR (nasal test) result (McAdam, 2020).',
   },
   other: {
     price: 100,
-    title: "Other",
-    id: "other",
-    info: "Other",
+    title: 'Other',
+    id: 'other',
+    info: 'Other',
   },
 };
